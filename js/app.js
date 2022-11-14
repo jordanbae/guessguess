d8ac4e9f04b57a87b3563dc747b857eed56df6f4
jordanbae <laphon.nala@bumail.net>
2022-11-14 23:35:37 +0700

    Add: Pass/Correct button logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 console.log('connected');
 
+let passButton = document.querySelector('.btn-pass');
+let correctButton = document.querySelector('.btn-correct');
+let imageContent = document.querySelector('.img-content')
 
-let tempArray = ['1','2','3','4','5','6','7']
+
+let tempArray = ['1','2','3','4','5']
+let scores = 0;
+
+passButton.addEventListener('click', () => {
+    console.log('pass clicked')
+    let newArr = [];
+    tempArray.push(tempArray.shift());
+    newArr = tempArray;
+    console.log(newArr)
+    imageContent.innerText = newArr[0];
+})
+
+correctButton.addEventListener('click', () => {
+    console.log('correct clicked')
+    let newArr2 = [];
+    tempArray.push(tempArray.shift());
+    newArr2 = tempArray;
+
+})
