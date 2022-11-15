8d0ae4c3d76631ce148d605a34de379cf0c40f9a
jordanbae <laphon.nala@bumail.net>
2022-11-16 00:25:59 +0700

    Add: Array Shuffler

--- js/app.js
+++ js/app.js
@@ -8,16 +8,13 @@ import movies from '../movies.json' assert {type: 'json'};
 
 let passButton = document.querySelector('.btn-pass');
 let correctButton = document.querySelector('.btn-correct');
-let movieNameHtml = document.querySelector('.content')
+let movieNameHtml = document.querySelector('.movie-name')
 let h1Content = document.querySelector('h1');
 
 
 let moviesList = [];
+
 let scores = 0;
-const preload = () => {
-    addTitleToMoviesList();
-    movieNameHtml.innerText = moviesList[0];
-}
 
 const addTitleToMoviesList = () => {
     movies.forEach(title => {
@@ -25,15 +22,28 @@ const addTitleToMoviesList = () => {
     })
 }
 
-preload();
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
 
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
-console.log(moviesList)
+const main = () => {
+    addTitleToMoviesList();
+    shuffleArray(moviesList)
+    movieNameHtml.innerHTML = moviesList[0];
+}
 
+main();
 
-
-
-
+// console.log(moviesList)
 
 passButton.addEventListener('click', () => {
     moviesList.push(moviesList.shift());
