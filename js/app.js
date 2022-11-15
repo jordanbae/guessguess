ced016cd47e758a6c4172f9a7df0abc31d27835d
jordanbae <laphon.nala@bumail.net>
2022-11-15 23:20:45 +0700

    Remove: All api calls

--- js/app.js
+++ js/app.js
@@ -1,95 +1,50 @@
 console.log('connected');
+import movies from '../movies.json' assert {type: 'json'};
 
-let movieList = {};
-let movieListArray = [];
+
+
+
+
 
 let passButton = document.querySelector('.btn-pass');
 let correctButton = document.querySelector('.btn-correct');
-let imageContent = document.querySelector('.content')
+let movieNameHtml = document.querySelector('.content')
 let h1Content = document.querySelector('h1');
 
 
-let tempArray = ['1','2','3','4','5']
-var scores = 0;
-let newArr = [];
+let moviesList = [];
+let scores = 0;
+const preload = () => {
+    addTitleToMoviesList();
+    movieNameHtml.innerText = moviesList[0];
+}
 
-
-const moviesId = {
-  method: 'GET',
-  url: 'https://imdb8.p.rapidapi.com/title/get-most-popular-movies',
-  params: {homeCountry: 'US', purchaseCountry: 'US', currentCountry: 'US'},
-  headers: {
-    'X-RapidAPI-Key': '42263d648emshbe13979451f9429p1dbc56jsn2f9b78bea88b',
-    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-  }
-};
-
-axios.request(moviesId)
-.then(function (response) {
-	// console.log('response.data', typeof response.data, response.data);
-    movieList = { ...response }
-    movieListArray = movieList.data;
-    // console.log("THIS IS A MOVIE LIST ARRAY", movieListArray, Array.isArray(movieListArray))
-    // console.log("movieList", Array.isArray(movieList.data) , movieList.data)
-    passFunction();
-})
-
-.catch(function (error) {
-	console.error(error);
-});
-    
-
-
-
-const passFunction = () => {
-
-
-    passButton.addEventListener('click', () => {
-        let modifiedIdsArray = [];
-        movieListArray.forEach((ids) => {
-            let modifiedIds = ids.replace('/title/', '');
-            modifiedIdsArray.push(modifiedIds);
-        })
-        let randId = modifiedIdsArray[Math.floor(Math.random()*modifiedIdsArray.length)];
-        console.log(randId)
-
-        const moviesTitle = {
-            method: 'GET',
-            url: 'https://imdb8.p.rapidapi.com/title/get-meta-data',
-            params: {ids: randId, region: 'US'}, //added modified ids, could crash
-            headers: {
-              'X-RapidAPI-Key': '42263d648emshbe13979451f9429p1dbc56jsn2f9b78bea88b',
-              'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-            }
-          };
-          
-          axios.request(moviesTitle).then(function (response) {
-              console.log(response.data);
-          }).catch(function (error) {
-              console.error(error);
-          });
-    
+const addTitleToMoviesList = () => {
+    movies.forEach(title => {
+        moviesList.push(title.title);
     })
 }
 
-// passButton.addEventListener('click', () => {
-    
+preload();
 
-    // console.log('pass clicked')
-    // tempArray.push(tempArray.shift());
-    // newArr = tempArray;
-    // console.log(newArr)
-    // imageContent.innerText = newArr[0];
 
-    // console.log('dis is movieList', movieList)
-// })
+console.log(moviesList)
+
+
+
+
+
+
+passButton.addEventListener('click', () => {
+    moviesList.push(moviesList.shift());
+    movieNameHtml.innerText = moviesList[0];
+    console.log('dis is movieList', moviesList)
+})
 
 correctButton.addEventListener('click', () => {
-    console.log('correct clicked')
-    tempArray.push(tempArray.shift());
-    newArr = tempArray;
-    console.log(newArr)
-    imageContent.innerText = newArr[0];
+    moviesList.push(moviesList.shift());
+    movieNameHtml.innerText = moviesList[0];
+    console.log('dis is movieList', moviesList)
     scores++
     h1Content.innerText = `${'Scores:' + scores}`
 })
