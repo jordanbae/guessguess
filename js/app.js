5929aac0957018c147b472ebeae118c1359b58aa
jordanbae <laphon.nala@bumail.net>
2022-11-16 23:00:26 +0700

    Add: Timer, Local Storage(Unfinish)

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ import movies from '../assets/movies.json' assert {type: 'json'};
 
 // TODO
 //
-// RE-ARRANGE FILES - REMOVING CATEGORY PAGE / COPY CONTENT APP.HTML TO INDEX.HTML
 // LOCAL STORAGE
 // MODAL
 //
@@ -14,15 +13,21 @@ let correctButton = document.querySelector('.btn-correct');
 let movieNameHtml = document.querySelector('.movie-name')
 let h1Content = document.querySelector('h1');
 
+let introModalBtn = document.querySelector('#intro-btn-closemodal');
+let introModalContainer = document.querySelector('.intro-modal-container');
+
 let modalbtnCloseEl = document.querySelector('#btn-closemodal')
-let modalContainer = document.querySelector('.modal-container');
+let modalContainer = document.querySelector('#modal_container');
+let modalPoints = document.querySelector('#modal-title');
+let modalInput = document.querySelector('#modal-input');
+
+
 
 let tempmodalbutton1 = document.querySelector('#tempbutton1');
-let tempmodalbutton2 = document.querySelector('#tempbutton2');
+let tempmodalbutton2 = document.querySelector('#btn-closemodal');
 
-
-// tempmodalbutton1.addEventListener('click', openEndModal);
-// tempmodalbutton2.addEventListener('click', closeEndModal);
+//Score
+let scores = 0;
 
 
 // modalCloseEl.addEventListener('click', () => {
@@ -31,14 +36,62 @@ let tempmodalbutton2 = document.querySelector('#tempbutton2');
 
 //Movies
 let moviesList = [];
+let playerList = [];
 // Modal Trigger functions;
+let currentDate = new Date();
+var datetime = "Date: " + currentDate.getDate() + "/"
+                + (currentDate.getMonth()+1)  + "/" 
+                + currentDate.getFullYear() + " - "  
+                + currentDate.getHours() + ":"  
+                + currentDate.getMinutes() + ":" 
+                + currentDate.getSeconds();
+
+
+
+const loadPlayerFromLocalStorage = () => {
+    let LoadplayerInfo = JSON.parse(localStorage.getItem(playerList));
+
+    if (LoadplayerInfo) {
+        datetime = LoadplayerInfo.date;
+        modalInput.value = LoadplayerInfo.name;
+        scores = LoadplayerInfo.points;
+    }
+    console.log(LoadplayerInfo);
+}
+
+const savePlayertoLocalStorage = () => {
+    let date = datetime;
+    let name = modalInput.value;
+    let points = scores;
+
+    let playerInfo = {
+        date: date,
+        name: name,
+        points: points,
+    }
+    playerList.push(playerInfo);
+    localStorage.setItem("playerInfo", JSON.stringify(playerList));
+}
+
+
 
 const openEndModal = () => {
-    modalContainer.style.dislay ='flex';
+    console.log('inopen')
+    modalContainer.style.display ='flex';
+    modalPoints.innerText = `${scores + ' Points'}`
+    // console.log("openEndModal - modalContainer.style.dislay", modalContainer.style.display)
+
 }
 
 const closeEndModal = () => {
+    console.log('inclose');
     modalContainer.style.display = 'none'
+
+    savePlayertoLocalStorage();
+}
+
+const closeIntroModal = () => {
+    introModalContainer.style.display = 'none';
 }
 
 //Timer
@@ -46,12 +99,6 @@ const startingTime = 1;
 let time = startingTime * 10;
 const countdownEl = document.querySelector('#countdown');
 console.log(countdownEl)
-
-
-//Score
-let scores = 0;
-
-
 
 //Timer
 const updateCountdown = () => {
@@ -62,10 +109,10 @@ const updateCountdown = () => {
 
     if(time === -1){
         clearInterval(timeInterval);
-        alert('times up');
+        openEndModal();
     }
 }
-// let timeInterval = setInterval(updateCountdown, 1000);
+let timeInterval = setInterval(updateCountdown, 1000);
 
 //Add movies to array
 const addTitleToMoviesList = () => {
@@ -114,6 +161,11 @@ correctButton.addEventListener('click', () => {
     movieNameHtml.innerText = moviesList[0];
     console.log('dis is movieList', moviesList)
     scores++
-    h1Content.innerText = `${'Scores:' + scores}`
+    h1Content.innerText = `${'Points: ' + scores}`
 })
+
+introModalBtn.addEventListener('click', closeIntroModal);
+
+tempmodalbutton1.addEventListener('click', openEndModal);
+tempmodalbutton2.addEventListener('click', closeEndModal);
 
