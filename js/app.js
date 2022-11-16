38eca7d39598aaf0b7f24dd9f018a78210d2cc2c
jordanbae <laphon.nala@bumail.net>
2022-11-16 16:37:28 +0700

    Add: Non-funtional Modal

--- js/app.js
+++ js/app.js
@@ -9,45 +9,72 @@ import movies from '../movies.json' assert {type: 'json'};
 // MODAL
 //
 
-
-
 let passButton = document.querySelector('.btn-pass');
 let correctButton = document.querySelector('.btn-correct');
 let movieNameHtml = document.querySelector('.movie-name')
 let h1Content = document.querySelector('h1');
 
+let modalbtnCloseEl = document.querySelector('#btn-closemodal')
+let modalContainer = document.querySelector('.modal-container');
 
+let tempmodalbutton1 = document.querySelector('#tempbutton1');
+let tempmodalbutton2 = document.querySelector('#tempbutton2');
+
+
+tempmodalbutton1.addEventListener('click', openEndModal);
+tempmodalbutton2.addEventListener('click', closeEndModal);
+
+
+// modalCloseEl.addEventListener('click', () => {
+//     console.log('clicked');
+// })
+
+//Movies
 let moviesList = [];
+// Modal Trigger functions;
 
+const openEndModal = () => {
+    modalContainer.style.dislay ='flex';
+}
+
+const closeEndModal = () => {
+    modalContainer.style.display = 'none'
+}
+
+//Timer
 const startingTime = 1;
 let time = startingTime * 10;
-
 const countdownEl = document.querySelector('#countdown');
 console.log(countdownEl)
 
+
+//Score
 let scores = 0;
 
 
+
+//Timer
 const updateCountdown = () => {
-    const minutes = Math.floor(time/60);
     let seconds = time % 60;
 
     countdownEl.innerHTML = `${seconds}`
     time--;
 
-    if(time === 0){
+    if(time === -1){
         clearInterval(timeInterval);
-        console.log('cleared');
+        alert('times up');
     }
 }
-let timeInterval = setInterval(updateCountdown, 1000);
+// let timeInterval = setInterval(updateCountdown, 1000);
 
+//Add movies to array
 const addTitleToMoviesList = () => {
     movies.forEach(title => {
         moviesList.push(title.title);
     })
 }
 
+//Shuffle array
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
@@ -61,16 +88,21 @@ const shuffleArray = (array) => {
   }
 }
 
+
+
+//MAIN FUNCTION --- THE START OF EVERYTHING
 const main = () => {
     addTitleToMoviesList();
     shuffleArray(moviesList)
     movieNameHtml.innerHTML = moviesList[0];
 }
-
 main();
 
-// console.log(moviesList)
 
+
+
+
+//BUTTONS EVENT
 passButton.addEventListener('click', () => {
     moviesList.push(moviesList.shift());
     movieNameHtml.innerText = moviesList[0];
